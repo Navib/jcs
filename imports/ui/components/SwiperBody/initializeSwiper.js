ac83dad68dfbdbcefca877344aefda06f5f7c46a
navib <ivan.baena.dev@gmail.com>
2017-11-18 15:52:08 -0500

    meteor update

--- imports/ui/components/SwiperBody/initializeSwiper.js
+++ imports/ui/components/SwiperBody/initializeSwiper.js
@@ -12,17 +12,16 @@ function initializeSwiper(){
       renderCustom: function (swiper, current, total) {
 
         var names = [
-             {id:1 ,name:'Justin-1',src:'http://via.placeholder.com/350x150?text=Justin-1'},
-             {id:2 ,name:'Justin-2',src:'http://via.placeholder.com/350x150?text=Justin-2'},
-             {id:3 ,name:'Justin-3',src:'http://via.placeholder.com/350x150?text=Justin-3'},
-             {id:4 ,name:'Justin-4',src:'http://via.placeholder.com/350x150?text=Justin-4'},
-             {id:5 ,name:'Justin-5',src:'http://via.placeholder.com/350x150?text=Justin-5'},
+             {id:1 ,name:'Justin-1',src:'http://via.placeholder.com/100x100?text=Justin-1'},
+             {id:2 ,name:'Justin-2',src:'http://via.placeholder.com/100x100?text=Justin-2'},
+             {id:3 ,name:'Justin-3',src:'http://via.placeholder.com/100x100?text=Justin-3'},
+             {id:4 ,name:'Justin-4',src:'http://via.placeholder.com/100x100?text=Justin-4'},
+             {id:5 ,name:'Justin-5',src:'http://via.placeholder.com/100x100?text=Justin-5'},
           ];
 
        var text = "<span class='cs-picker' style='background-color:black;padding:20px;'>";
          $.each(names, function( key, value ) {
-console.log(value.src);
-
+            console.log(value.src);
              if (current == key) {
                text += "<span style='border-top:1px solid green;margin-right:4px;color:green;padding:10px;'><img src="+value.src+" /></span>";
              } else {
@@ -31,9 +30,9 @@ console.log(value.src);
          });
          text += "</span>";
          return text;
-      }
-    },
-    mousewheel: {
+       }
+      },
+      mousewheel: {
       invert: true,
     },
   });
