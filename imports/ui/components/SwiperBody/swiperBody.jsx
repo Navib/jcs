baf8c33b6dd94ab288256c0b51d5758d49ba3725
navib <ivan.baena.dev@gmail.com>
2017-11-20 14:16:13 -0500

    pa bound

--- imports/ui/components/SwiperBody/swiperBody.jsx
+++ imports/ui/components/SwiperBody/swiperBody.jsx
@@ -28,13 +28,11 @@ class SwiperBody extends Component {
       <div className="actor-profile-wrapper">
         <div className="swiper-container">
           <div className="swiper-wrapper">
-            <div className="swiper-slide">
-              <div className=""><img src="/images/face-shot.jpg" className="main-swiper-img"/></div>
-            </div>
+            <div className="swiper-slide"><img src="/images/ohlala.jpg" className="main-swiper-img"/></div>
+            <div className="swiper-slide"><div className=""><img src="/images/face-shot.jpg" className="main-swiper-img"/></div></div>
             <div className="swiper-slide"><img src="/images/jcs-actor.jpg" className="main-swiper-img"/></div>
             <div className="swiper-slide"><img src="/images/behind-the-scenes.png" className="main-swiper-img"/></div>
             <div className="swiper-slide"><img src="/images/on-set-jcs.jpg" className="main-swiper-img"/></div>
-            <div className="swiper-slide"><img src="/images/ohlala.jpg" className="main-swiper-img"/></div>
           </div>
 
           <div className="swiper-pagination"></div>
--- imports/ui/components/TextBox/textBox.jsx
+++ imports/ui/components/TextBox/textBox.jsx
@@ -14,7 +14,20 @@ class TextBox extends Component {
         <div className="jcs-content">
           <div className="jcs-bx-header">Justin C. Schilling</div>
           <div className="jcs-bx-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </div>
-          <div className="jcs-bx-social">Justin C. Schilling</div>
+          <div className="jcs-bx-social">
+            <ul className="actor-sites">
+              <li className="social-bg"></li>
+              <li className="social-bg"></li>
+            </ul>
+          </div>
+          <div className="jcs-menu-icon">
+            <ul className="social-list">
+              <li><i className="icon ion-ios-film" aria-hidden="true"></i></li>
+              <li><i className="icon ion-film-marker" aria-hidden="true"></i></li>
+              <li><i className="icon ion-social-vimeo" aria-hidden="true"></i></li>
+              <li><i className="ion-ios-email-outline" aria-hidden="true"></i></li>
+            </ul>
+          </div>
         </div>
       </div>
     )
